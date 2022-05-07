067e2a494faf46ca06a3b41c313da2e7c19b5e59
Jarosław Bogdański <mordka4428@gmail.com>
2022-05-07 21:46:23 +0200

    currency converter correction

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 {
-    const formElement = document.querySelector(".js-form");
-   
-
-    formElement.addEventListener("input", () => {
+    const onFormSubmit = () => {
         const inputElement = document.querySelector(".js-input");
         const outputElement = document.querySelector(".js-output");
         const firstElement = document.querySelector(".js-first");
         const secondElement = document.querySelector(".js-second");
+        calculateResult(inputElement, outputElement, firstElement, secondElement);
+
+    };
+    const calculateResult = (inputElement, outputElement, firstElement, secondElement) => {
         const input = inputElement.value;
         const firstValue = firstElement.value;
         const secondValue = secondElement.value;
         const output = (input * firstValue) / secondValue;
         outputElement.value = output.toFixed(2);
-    });
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("input", onFormSubmit);
+
+    }
+    init();
 }
+
 
 
 
